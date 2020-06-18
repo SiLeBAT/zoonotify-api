ea8ad21f7b528d7a90b0e63630510614449a8987
Dominic Tölle <dominic.tolle@gmail.com>
2020-06-18 09:31:57 +0200

    Change startapp.sh script so it works
    Change-Id: Ib6256bd887d561484026c8fd82bfedff578f7183
    Ticket: #6

--- startapp.sh
+++ startapp.sh
@@ -4,12 +4,10 @@ source ./environment.sh
 
 LOG_FILE=$1
 if [ "$#" -ne 1 ]; then
-  LOG_FILE=./zoonotify_output
+  LOG_FILE=./zoonotify.log
 fi
 
 BASE_NAME=`basename $LOG_FILE .log`
 DIR_NAME=`dirname $LOG_FILE`
-ADMIN_LOG=$DIR_NAME/$BASE_NAME-admin.log
 
 NODE_ENV=$ZOONOTIFY_NODE_ENV HOST=$ZOONOTIFY_HOST forever -l $LOG_FILE -a start ./lib/main.js
-forever -l $ADMIN_LOG -a start ./node_modules/mongo-express/app.js
