3d68793432a544e69dbedd5568152f450c594dd0
Dominic Tölle <dominic.tolle@gmail.com>
2021-02-02 10:59:01 +0100

    Add log output to microorganism gateway
    Ticket: #59
    Change-Id: I5f8918a0b1b1ebc7f682bca8765c6997b6cce9be

--- src/infrastructure/persistence/gateway/microorganism.gateway.ts
+++ src/infrastructure/persistence/gateway/microorganism.gateway.ts
@@ -4,6 +4,7 @@ import { MicroorganismModel } from '../dao/microorganism.model';
 import { ModelStatic } from '../dao/shared.model';
 import { PERSISTENCE_TYPES } from '../persistence.types';
 import { Microorganism, MicroorganismGateway } from '../../../app/ports';
+import { logger } from './../../../aspects';
 
 @injectable()
 export class SequelizeMicroorganismGateway implements MicroorganismGateway {
@@ -13,12 +14,21 @@ export class SequelizeMicroorganismGateway implements MicroorganismGateway {
     ) {}
 
     findAll(): Promise<Microorganism[]> {
-        return this.Microorganism.findAll().then(model =>
-            model.map(m => this.toEntity(m))
+        logger.trace(
+            `${this.constructor.name}.${this.findAll.name}, Executing`
         );
+        return this.Microorganism.findAll()
+            .then(model => model.map(m => this.toEntity(m)))
+            .catch(error => {
+                logger.error(error);
+                throw error;
+            });
     }
 
     private toEntity(model: MicroorganismModel): Microorganism {
+        logger.trace(
+            `${this.constructor.name}.${this.toEntity.name}, Executing`
+        );
         return {
             name: model.name
         };
