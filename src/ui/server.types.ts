90628a4016a708c88e819f90524f7593827bf825
Dominic Tölle <dominic.tolle@gmail.com>
2022-01-25 09:46:41 +0100

    Add all AMRs to isolate dataset
    Ticket: #133

--- src/ui/server.types.ts
+++ src/ui/server.types.ts
@@ -4,7 +4,6 @@ const SERVER_TYPES = {
     DatabaseController: Symbol.for('DatabaseController'),
     FilterConfigController: Symbol.for('FilterConfigController'),
     IsolateController: Symbol.for('IsolateController'),
-    MockDataController: Symbol.for('MockDataController'),
     VersionRootController: Symbol.for('VersionRootController'),
     APIDocsController: Symbol.for('APIDocsController'),
     SwaggerMW: Symbol.for('SwaggerMW'),
